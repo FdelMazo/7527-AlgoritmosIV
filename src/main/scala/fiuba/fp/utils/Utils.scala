cdce0f397dbcc506596916ad6ba0d8be6aee5a59
FdelMazo <fdelmazo@fi.uba.ar>
2020-11-05 14:34:46 -0300

    Add correct DB column types

--- src/main/scala/fiuba/fp/utils/Utils.scala
+++ src/main/scala/fiuba/fp/utils/Utils.scala
@@ -2,7 +2,7 @@ package fiuba.fp.utils
 
 final case object Utils {
   def split(str: String): List[String] = {
-
+    // split a mano -> Mas divertido de codear que .split()
     val seed: List[List[Char]] = List(Nil)
 
     val listOfStrings: List[List[Char]] = str.foldRight[List[List[Char]]](seed)((e, acc) => {
