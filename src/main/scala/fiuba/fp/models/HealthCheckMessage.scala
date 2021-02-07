2825f2e2b2b49ff9d25c9cd435f8be7eb2819efc
FdelMazo <fdelmazo@fi.uba.ar>
2021-02-07 15:27:07 -0300

    Agrega skel TP3

---
+++ src/main/scala/fiuba/fp/models/HealthCheckMessage.scala
@@ -0,0 +1,10 @@
+package fiuba.fp.models
+
+import io.circe._
+import io.circe.generic.semiauto._
+
+final case class HealthCheckMessage(version: String, maintainer: String)
+
+object HealthCheckMessage {
+  implicit val encoder: Encoder[HealthCheckMessage] = deriveEncoder
+}
---
+++ src/main/scala/fiuba/fp/services/HealthCheck.scala
@@ -0,0 +1,14 @@
+package fiuba.fp.services
+
+import cats.Applicative
+import cats.implicits._
+import fiuba.fp.models.HealthCheckMessage
+
+trait HealthCheck[F[_]] {
+  def healthCheck: F[HealthCheckMessage]
+}
+
+class HealthCheckImpl[F[_]: Applicative](team: String) extends HealthCheck[F] {
+  override def healthCheck: F[HealthCheckMessage] =
+    HealthCheckMessage("0.1",team).pure[F]
+}
