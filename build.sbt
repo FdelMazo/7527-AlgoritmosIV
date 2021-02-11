9ad9ec54b339135896fae3bf515df9c18d0a9b15
CrossNox <ijmermet@gmail.com>
2021-02-11 10:13:57 -0300

    fixes

--- build.sbt
+++ build.sbt
@@ -7,29 +7,12 @@ val doobieVersion = "0.9.4"
 val postgresVersion = "42.2.18"
 val xmlVersion = "2.3.2"
 
-name := "fptp"
+organization := "edu.fiuba"
 
-version := "0.0.1"
+name := "fp-fiuba-4-3"
 
-scalaVersion := "2.12.12"
+version := "0.0.1-SNAPSHOT"
 
-libraryDependencies ++= Seq(
-  "ch.qos.logback"     %  "logback-classic"      % LogbackVersion,
-  "co.fs2"             %% "fs2-io"               % "2.3.0",
-  "io.circe"           %% "circe-generic"        % CirceVersion,
-  "jakarta.xml.bind"   %  "jakarta.xml.bind-api" % xmlVersion,
-  "org.apache.spark"   %% "spark-mllib"          % "3.0.1",
-  "org.glassfish.jaxb" %  "jaxb-runtime"         % xmlVersion,
-  "org.http4s"         %% "http4s-blaze-client"  % Http4sVersion,
-  "org.http4s"         %% "http4s-blaze-server"  % Http4sVersion,
-  "org.http4s"         %% "http4s-circe"         % Http4sVersion,
-  "org.http4s"         %% "http4s-dsl"           % Http4sVersion,
-  "org.jpmml"          %  "pmml-evaluator"       % pmmlVersion,
-  "org.jpmml"          %  "jpmml-sparkml"        % "1.6.1",
-  "org.postgresql"     %  "postgresql"           % postgresVersion,
-  "org.scalatest"      %% "scalatest"            % "3.2.0",
-  "org.specs2"         %% "specs2-core"          % Specs2Version % Test,
-  "org.tpolecat"       %% "doobie-hikari"        % doobieVersion,
-  "org.tpolecat"       %% "doobie-core"          % "0.9.0",
-)
+scalaVersion := "2.13.3"
+
 
--- src/main/scala/fiuba/fp/services/ScoreService.scala
+++ src/main/scala/fiuba/fp/services/ScoreService.scala
@@ -1,46 +1,41 @@
 package fiuba.fp.services
 
-import doobie.implicits._
 import cats.Applicative
 import cats.effect.IO
 import cats.implicits._
+import doobie.implicits._
 import doobie.util.transactor.Transactor
-import fiuba.fp.Main.{transaction, s1, assembler, args}
 import fiuba.fp.models.{DataSetRow, Score, ScoreMessage, InputRow}
-import org.jpmml.evaluator.{Evaluator,LoadingModelEvaluatorBuilder}
-import org.dmg.pmml.{FieldName}
-import org.jpmml.evaluator.{FieldValue,EvaluatorUtil}
 import java.io.File
-import scala.concurrent.ExecutionContext
+import org.dmg.pmml.{FieldName, PMML}
+import org.jpmml.evaluator.{FieldValue,EvaluatorUtil,LoadingModelEvaluatorBuilder}
 import scala.collection.JavaConverters._
-
-
-
+import scala.concurrent.ExecutionContext
 
 trait ScoreService[F[_]] {
   def score(data: InputRow): F[ScoreMessage]
 }
 
 class ScoreServiceImpl[F[_]: Applicative]() extends ScoreService[F] {
-  def PMMLevaluate(data: InputRow): Double = {
-    val evaluator = new LoadingModelEvaluatorBuilder().load(new File("/home/nox/repos/fiuba/7527-algoritmos-4/7527-AlgoritmosIV/cosoide.pmml")).build();
+    def PMMLevaluate(data: InputRow): String = {
+      val evaluator = new LoadingModelEvaluatorBuilder().load(new File("cosoide.pmml")).build();
 
-    evaluator.verify();
+      evaluator.verify();
 
-    val dataMap : Map[String, Any] = data.getClass.getDeclaredFields.map(_.getName).zip(data.productIterator.to).toMap
+      val dataMap : Map[String, Any] = (data.productElementNames zip data.productIterator).toMap
+
+      val arguments : Map[FieldName, FieldValue] = (for {
+        inputField <- evaluator.getInputFields.asScala.toList
+        rawValue <- dataMap.get(inputField.getName.getValue)
+      } yield (
+        inputField.getName,
+        inputField.prepare(rawValue)
+      )).toMap
+
+      val results = evaluator.evaluate(arguments.asJava)
     
-    val arguments : Map[FieldName, FieldValue] = (for {
-      inputField <- evaluator.getInputFields.asScala.toList
-      rawValue <- dataMap.get(inputField.getName.getValue)
-    } yield (
-      inputField.getName,
-      inputField.prepare(rawValue)
-    )).toMap
-
-    val results = evaluator.evaluate(arguments.asJava)
-    
-    0.7
-  }
+      "0.7"
+    }
 
   override def score(data: InputRow): F[ScoreMessage] = {
     implicit val cs = IO.contextShift(ExecutionContext.global)
@@ -48,27 +43,20 @@ class ScoreServiceImpl[F[_]: Applicative]() extends ScoreService[F] {
     val transactor = Transactor.fromDriverManager[IO](
       "org.postgresql.Driver",
       s"jdbc:postgresql://localhost/fiuba",
-      "fiuba", "fiuba")
+      "fiuba", "fiuba"
+    )
 
-    //val datasetrow = DataSetRow.selectById(data.id)
-    //  .compile
-    //  .toList
-    //  .transact(transactor)
-    //  .unsafeRunSync
-    //  .head
-    /*
-    val scorerow = Score.selectByHash(data.hashCode())
+    val scorerow: Option[Score] = Score.selectByHash(data.hashCode)
       .compile
       .toList
-      .transact(transactor).option
+      .transact(transactor)
       .unsafeRunSync
+      .headOption
 
     val score = scorerow match {
-      case Some(h) => h
+      case Some(h) => h.score
       case _ => PMMLevaluate(data)
     }
-   */
-    val score : Double = PMMLevaluate(data)
 
     ScoreMessage("La la la").pure[F]
   }
